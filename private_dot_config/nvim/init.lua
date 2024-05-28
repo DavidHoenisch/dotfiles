bcf930621c8c9a67499dc19cef9a8e5ffd4b6332
DavidHoenisch <dh1689@pm.me>
2024-05-27 22:36:13 -0700

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .local/bin/go_setup Update .local/bin/wscreen

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -481,6 +481,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        terraformls = {},
         --pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -520,7 +521,8 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
+        'stylua',
+        'tflint', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
