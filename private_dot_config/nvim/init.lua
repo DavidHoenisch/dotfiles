6e04e2b4c3f2ef7eb73ede972b1dc5e49bef4911
DavidHoenisch <dh1689@pm.me>
2024-05-28 11:40:13 -0700

    Update .zshrc Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -482,6 +482,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         terraformls = {},
+        actionlint = {},
         --pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
