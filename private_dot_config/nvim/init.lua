abf12891ac86a28a32116c2630b0250ac9f99aaa
DavidHoenisch <dh1689@pm.me>
2024-07-25 18:20:48 -0700

    Update .profile Update .zshrc Update .config/espanso/match/base.yml Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/sway/config Update .local/bin/oserve Update .local/bin/wscreen

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -492,6 +492,10 @@ require("lazy").setup({
 				pyright = {},
 				rust_analyzer = {},
 				clangd = {},
+				prettier = {},
+				yamllint = {},
+				yamlls = {},
+				yamlfix = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
